0c8676584b7d6bb40e06946e8288ef1079f6aee4
Alex Sanchez <sanchezcalexander@gmail.com>
2024-02-13 04:30:16 -0800

    add_correct done, progress on add_halts

--- FRACTRAN/Test.lean
+++ FRACTRAN/Test.lean
@@ -17,9 +17,7 @@ def next' (prog : FProg) (n : Int) : Option Int :=
 def next (prog : FProg) (n : Int) : Int :=
   match prog with
   | []      => 0
-  | q :: qs => if Rat.isInt (q * n)
-               then (q * n).num
-               else next qs n
+  | q :: qs => cond (Rat.isInt (q * n)) (q * n).num $ next qs n
 
 unsafe def runProg' (prog : FProg) : FRun' :=
   fun z ↦ match (next' prog z) with
@@ -31,14 +29,37 @@ def runProg (prog : FProg) : FRun :=
             | Nat.zero   => z
             | Nat.succ k => next prog $ runProg prog z k
 
-def n : Int := 2 ^ 3 * 3 ^ 7
-#eval LazyList.toList $ runProg' [Rat.mk' 2 3] n
-#eval runProg [Rat.mk' 2 3] n 7
+--def n : Int := 2 ^ 3 * 3 ^ 7
+--#eval LazyList.toList $ runProg' [(2 : Rat) / 3] n
+--#eval runProg [(2 : Rat) / 3] n 7
 
-def adder (a b : Nat) := runProg [Rat.mk' 2 3] (2^a * 3^b)
+def adder (a b : Nat) := runProg [(2 : Rat) / 3] (2^a * 3^b)
 
-lemma add_once {m : Int} : next [Rat.mk' 2 3] (3 * m) = 2 * m := by
-  sorry
+lemma add_once {m : Int} : next [(2 : Rat) / 3] (3 * m) = 2 * m := by
+  conv =>
+    lhs
+    unfold next
+    unfold next
+    simp
+    conv =>
+      congr
+      · rhs
+        rw [← mul_assoc]
+        conv =>
+          lhs
+          rw [div_mul, div_self (by norm_num), div_one]
+        change (↑ (Int.ofNat 2)) * ↑ m
+        rw [(Int.cast_mul (Int.ofNat 2) m).symm]
+        change ↑(2 * m)
+      · rhs
+        rw [← mul_assoc]
+        conv =>
+          lhs
+          rw [div_mul, div_self (by norm_num), div_one]
+        change (↑ (Int.ofNat 2)) * ↑ m
+        rw [(Int.cast_mul (Int.ofNat 2) m).symm]
+        change ↑(2 * m)
+      · skip
 
 lemma add_some {a b c : Nat} (h : c ≤ b) : adder a b c = 2 ^ (a + c) * 3 ^ (b - c) := by
   induction' c with c ih
@@ -48,7 +69,7 @@ lemma add_some {a b c : Nat} (h : c ≤ b) : adder a b c = 2 ^ (a + c) * 3 ^ (b 
       change 2 ^ a * 3 ^ b
   · conv =>
       congr
-      · change next [Rat.mk' 2 3] $ adder a b c
+      · change next [(2 : Rat) / 3] $ adder a b c
         rw [ih $ le_trans (Nat.le.step Nat.le.refl) h]
         rhs
         conv =>
@@ -86,7 +107,20 @@ lemma add_correct (a b : Nat) : adder a b b = 2 ^ (a + b) := by
     rw [mul_one]
 
 lemma add_halts {a n N : Nat} (h : n > N) (last : adder a N N = 2 ^ (a + N)) : adder a N n = 0 := by
-  sorry
+  induction' n with n ih
+  · exfalso
+    exact (Nat.not_lt_zero N) h
+  · unfold adder
+    unfold runProg
+    conv =>
+      lhs
+      rhs
+      change adder a N n
+    by_cases h' : n = N
+    · rw [h', last]
+      sorry
+    · rw [ih ∘ Ne.lt_of_le' h' ∘ Nat.lt_succ.mp $ h]
+      sorry
 
 theorem adder_adds : ∀ a b : Nat, ∃ K : Nat, (adder a b K = 2^(a + b) ∧ ∀ n : Nat, n > K → adder a b n = 0) := by
   intro a b
